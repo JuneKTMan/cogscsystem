c6e61feb556e2dcc1ffb04ba0c6e5ec933fa2550
June Man <you@example.com>
2022-03-01 20:33:59 -0400

    CCCMS

---
+++ js/custom/dashboard.js
@@ -0,0 +1,35 @@
+$(function () {
+    //Json data by api call for order table
+    $.get(orderListApiUrl, function (response) {
+        if(response) {
+            var table = '';
+            var totalCost = 0;
+            $.each(response, function(index, order) {
+                totalCost += parseFloat(order.total_cost);
+                table += '<tr>' +
+                    '<td>'+ order.date_time +'</td>'+
+                    '<td>'+ order.order_id +'</td>'+
+                    '<td>'+ order.customer_name +'</td>'+
+                    '<td>'+ order.total_cost.toFixed(2) +' CA</td></tr>';
+                    '<td><span class="btn btn-xs btn-danger details-order">Details</span></td></tr>';
+            });
+            table += '<tr><td colspan="3" style="text-align: end"><b>Total</b></td><td><b>'+ totalCost.toFixed(2) +' CA</b></td></tr>';
+            $("table").find('tbody').empty().html(table);
+        }
+    });
+
+    $(document).on("click", ".details-order", function (){
+        var tr = $(this).closest('tr');
+        var data = {
+            product_id : tr.data('id')
+        };
+        var isDelete = confirm("Are you sure to delete "+ tr.data('name') +" item?");
+        if (isDelete) {
+            callApi("POST", productDeleteApiUrl, data);
+        }
+    });
+
+
+
+
+});
